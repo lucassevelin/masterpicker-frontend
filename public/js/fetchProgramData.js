39d5d0c83c7e6428e3cf829fc4665d2bb63c70c4
Lucas Sevelin <lucas.sevelin@gmail.com>
2022-12-22 21:27:57 +0100

    Test

---
+++ public/js/fetchProgramData.js
@@ -0,0 +1,83 @@
+function fetchProgramData(program, tableID) {
+    //var programCol = query((selectedCol + "/" + selectedProgram + "/courses")) //, orderBy('term', 'desc'))
+    var db = firebase.firestore();
+    var programCol = db.collection(db, selectedCol, program, "courses")
+    //console.log(programCol)
+
+    clearTable(tableID);
+    var tableToFill = document.getElementById(tableID).getElementsByTagName('tbody')[0]
+
+    onSnapshot(programCol, (snapshot) => {
+        snapshot.docs.forEach((doc) => {
+            //console.log("Data is of type: " + doc.data().constructor)
+            //console.log("Data is still of type: " + dataToPush.constructor)
+            //console.log(doc.data())
+            
+            // Insert a row at the end of table
+            var newRow = tableToFill.insertRow();
+
+            // Insert a cell at the end of the row
+            var newCell = newRow.insertCell();
+
+            // Append a text node to the cell
+            //var newText = document.createTextNode(doc.data().name);
+            if (doc.data().link) {
+                var newText = document.createElement("a")
+                newText.setAttribute("href", doc.data().link)
+                newText.setAttribute("target", "_blank")
+                newText.innerText = doc.data().name
+            } else {
+                var newText = document.createTextNode(doc.data().name)
+            }
+            newCell.appendChild(newText);
+
+            var newCell = newRow.insertCell();
+            var newText = document.createTextNode(doc.data().code);
+            newCell.appendChild(newText);
+
+            var newCell = newRow.insertCell();
+            var newText = document.createTextNode(doc.data().term);
+            newCell.appendChild(newText);
+
+            var newCell = newRow.insertCell();
+            var newText = document.createTextNode(doc.data().timetableModule);
+            newCell.appendChild(newText);
+
+            var newCell = newRow.insertCell();
+            var newText = document.createTextNode(doc.data().exam);
+            newCell.appendChild(newText);
+
+            var newCell = newRow.insertCell();
+            //console.log(doc.data().multiplePeriods)
+            if (doc.data().multiplePeriods) {
+                var newText = document.createTextNode(doc.data().credits);
+            } else {
+                var newText = document.createTextNode(String(doc.data().credits + "*"));
+            }
+            newCell.appendChild(newText);
+
+            var newCell = newRow.insertCell();
+            var newText = document.createTextNode(doc.data().ECV);
+            newCell.appendChild(newText);
+
+            var newCell = newRow.insertCell();
+            var newText = document.createTextNode(doc.data().fields);
+            newCell.appendChild(newText);
+
+            var newCell = newRow.insertCell();
+            var newText = document.createTextNode(doc.data().level);
+            newCell.appendChild(newText);
+
+            var newCell = newRow.insertCell();
+            var newCheckbox = document.createElement("input");
+            newCheckbox.type = "checkbox";
+            newCheckbox.setAttribute("onClick", "colorRow(this)");
+            newCell.appendChild(newCheckbox);
+            // if(doc.data().ECV.startsWith("E")) {
+            //     newCheckbox.checked = true;
+            //     colorRow(newCheckbox)
+            // }
+        })
+    })
+    console.log("Done")
+}
--- public/js/populateSelect.js
+++ public/js/populateSelect.js
@@ -1,4 +1,5 @@
 function populateSelect(collection, selectId){
+    console.log("Hej")
     var db = firebase.firestore();
     var data = db.collection(collection).get()
     .then(snapshot => {
@@ -6,7 +7,10 @@ function populateSelect(collection, selectId){
         // Now that you have the data, create an option element for each item
         var option = document.createElement('option');
         option.value = doc.id;  // Set the value of the option to the document ID
-        option.text = doc.data().name;  // Set the text of the option to the name field of the document
+        
+        //console.log(doc.id.slice(0,-8))
+        styledText = doc.id.slice(0,-8) + " (" + doc.id.slice(-7,-5).toUpperCase() + " " + doc.id.slice(-4) + ")"
+        option.text = styledText;  // Set the text of the option to the name field of the document
         
         // Append the option to the select element
         document.getElementById(selectId).appendChild(option);
@@ -15,4 +19,4 @@ function populateSelect(collection, selectId){
     .catch(error => {
         console.log(error);
     });
-}
+  }
